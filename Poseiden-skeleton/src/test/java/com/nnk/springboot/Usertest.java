8f02f8e578fc9a9311e2faf8004aee107bc9fa05
estated90 <nicolas.de-testa@hotmail.fr>
2020-12-13 12:04:56 +0100

    add testing logic

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/Usertest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/Usertest.java
@@ -1,5 +1,9 @@
 package com.nnk.springboot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -14,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.nnk.springboot.domain.User;
 
@@ -33,5 +38,7 @@ public class Usertest {
     @DisplayName("Full process User")
     void crun_full_process_user() throws Exception {
 	User user = new User("test1", "test1", "test1", "USER");
+	MvcResult mvcResult = mockMvc.perform(get("/user/list")).andExpect(status().isOk()).andReturn();
+	String jsonResponse = mvcResult.getResponse().getContentAsString();
     }
 }
